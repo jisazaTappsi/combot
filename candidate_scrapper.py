355b0dd140596b234f87b76e2bce2858f772e5f7
Juan Pablo <juan.isaza@voiq.com>
2018-11-04 01:36:29 -0300

    candidate scrapper

--- candidate_scrapper.py
+++ candidate_scrapper.py
@@ -10,12 +10,19 @@ import stringdist
 from os import listdir
 from os.path import isfile, join
 import os
+import re
+import time
+import pandas as pd
 
 import util
 
 EMAIL_ID = 'txEmail'
 PASS_ID = 'txPwd'
 CV_text = ' Hoja de vida de  '
+EMAIL = 'email'
+CAMPAIGN_ID = 'campaign_id'
+KEY = 'key'
+CSV_FILENAME = 'sent_candidates.csv'
 
 
 def load_browser_and_login(main_url):
@@ -56,46 +63,45 @@ def get_city(cities, city_field):
 
     city_name = util.remove_accents_in_string(city_field.lower().strip())
 
+    closest_match = None
+    closest_distance = 100
     for city in cities:
-        #print(db_city_name)
-        #print('distance: ' + str(stringdist.levenshtein(city_name, db_city_name)))
-        if stringdist.levenshtein(city_name, city['fields']['name']) < 4:  # if Levishtein distance is close enough will do!
-            return city
+        db_city_name = util.remove_accents_in_string(city['fields']['name'].lower().strip())
+        if city['fields']['alias']:
+            db_city_alias = util.remove_accents_in_string(city['fields']['alias'].lower().strip())
+        else:
+            db_city_alias = 'this is never ever a city...'
 
-    return None
+        distance = stringdist.levenshtein(city_name, db_city_name)
+        distance_alias = stringdist.levenshtein(city_name, db_city_alias)
+
+        if (distance < closest_distance and distance < 6) or \
+                (distance_alias < closest_distance and distance_alias < 6):  # if Levishtein distance is close enough will do!
+            closest_match = city
+            closest_distance = min(distance, distance_alias)
+            print('city: ' + db_city_name + ' distance: ' + str(closest_distance))
+
+    return closest_match
 
 
-def get_max_campaign_with_id(campaigns):
-    max_id = max([c['pk'] for c in campaigns])
-    return [c for c in campaigns if c['pk'] == max_id][0]
-
-
-def get_campaign(campaigns, profile_list_html):
-    """
-    Gets the last campaign, that its name is close enough.
-    :param campaigns:
-    :param profile_list_html:
-    :return:
-    """
+def get_campaign_with_id(campaigns, profile_list_html):
 
     campaign_name = profile_list_html.find('p', class_='gescan_subtit').span.text.replace('Oferta', '')
-
     campaign_name = util.remove_accents_in_string(campaign_name.lower().strip())
 
-    #print('campaign_name: ' + str(campaign_name))
-
-    possible_campaigns = []
-    for campaign in campaigns:
-        #print(campaign['pk'])
-        #print(campaign['fields']['title_es'])
-        if stringdist.levenshtein(campaign_name, campaign['fields']['title_es']) < 3:  # if Levishtein distance is close enough will do!
-            #print('added: ' + str(id))
-            possible_campaigns.append(campaign)
-
-    if len(possible_campaigns) > 0:
-        return get_max_campaign_with_id(possible_campaigns)
+    matches = re.findall(r'\d+', campaign_name)
+    if len(matches) > 0:
+        campaign_id = int(matches[-1])
     else:
+        print('could not find any match for campaign, will send candidate to Default Campaign :(')
         return None
+
+    for c in campaigns:
+        if int(c['pk']) == campaign_id:
+            return c
+
+    print('could not find any match for campaign, will send candidate to Default Campaign :(')
+    return None
 
 
 def get_name(profile_html):
@@ -112,7 +118,7 @@ def scrap_profile(html_source, cities, campaign):
 
     user = dict()
     if campaign:
-        user['campaign_id'] = campaign['pk']
+        user[CAMPAIGN_ID] = campaign['pk']
     if campaign and campaign['fields']['work_area']:
         user['work_area_id'] = campaign['fields']['work_area']
 
@@ -120,13 +126,11 @@ def scrap_profile(html_source, cities, campaign):
     if name:
         user['name'] = name
 
-    #user['name'] = profile_html.find_all('p', {'class': 'fl'}).text.replace(, '')
-
     emails = util.get_patterns(util.EMAIL_REGEX, html_source)
     emails = [t for t in emails if config('bolsa1_name') not in t and 'santiagopsa' not in t]
 
     if len(emails) > 0:
-        user['email'] = emails[0]
+        user[EMAIL] = emails[0]
 
     phones_obj = profile_soup.find_all('span', class_='fl fw_n mt3')
 
@@ -144,9 +148,6 @@ def scrap_profile(html_source, cities, campaign):
     salary_string = profile_soup.find('span', class_='icon salario').parent.text
 
     user['salary'] = int(salary_string.split('$')[1].strip().replace(',', '').split('.')[0])
-
-    print('user data:')
-    print(user)
 
     return user
 
@@ -166,7 +167,7 @@ def get_campaigns():
 
 
 def get_all_file_names(directory):
-    return [f for f in listdir(directory) if isfile(join(directory, f))]
+    return [f for f in listdir(directory) if isfile(join(directory, f)) and 'crdownload' not in f]
 
 
 def get_last_download_path():
@@ -177,14 +178,32 @@ def get_last_download_path():
     last_file = None
     file_time = 0
     for cv_path in [os.path.join(cv_directory, f) for f in all_files]:
-        if os.path.getmtime(cv_path) > file_time:
-            file_time = os.path.getmtime(cv_path)
-            last_file = cv_path
+        try:
+            if os.path.getmtime(cv_path) > file_time:
+                file_time = os.path.getmtime(cv_path)
+                last_file = cv_path
+        except FileNotFoundError:
+            pass
 
     if last_file:
         return os.path.join(cv_directory, last_file)
     else:
         return None
+
+
+def poll_for_last_download(current_cv_path):
+    """
+    Will wait for the download to finish, up to 5 min, else will output None
+    :param current_cv_path: This is the last path just before clicking
+    :return: a new path
+    """
+    timeout = 1*60
+
+    current_time = time.time()
+    while get_last_download_path() == current_cv_path and time.time() - current_time < timeout:
+        time.sleep(1)
+
+    return get_last_download_path() if get_last_download_path() != current_cv_path else None
 
 
 def download_file(url, file_path):
@@ -211,7 +230,7 @@ def download_and_get_cv_path(browser, user):
     for a_tag_child in children:
         if a_tag_child['href'] != '#':
             cv_url = get_complete_bolsa_url(a_tag_child['href'])
-            cv_path = os.path.join(config('path_to_cvs'), user['email'].replace('.', ''))
+            cv_path = os.path.join(config('path_to_cvs'), user[EMAIL].replace('.', ''))
             download_file(cv_url, cv_path)
             return cv_path
 
@@ -222,9 +241,25 @@ def get_complete_bolsa_url(href_url):
     return config('bolsa1_company_url') + href_url
 
 
+def get_key(user, campaign):
+    if campaign is not None:
+        return user[EMAIL] + ' ' + str(campaign['pk'])
+    else:
+        return user[EMAIL] + ' None'
+
+
+def candidate_not_sent(sent_candidates, user, campaign):
+    return sum(sent_candidates[KEY] == get_key(user, campaign)) == 0
+
+
 def run():
     cities = get_cities()
     campaigns = get_campaigns()
+
+    try:
+        sent_candidates = pd.read_csv(CSV_FILENAME, sep=',')
+    except FileNotFoundError:
+        sent_candidates = pd.DataFrame(columns=[EMAIL, CAMPAIGN_ID, KEY])
 
     browser = load_browser_and_login(config('bolsa1_url'))
 
@@ -239,7 +274,7 @@ def run():
             browser.get(url)
             profile_list_html = BeautifulSoup(browser.page_source, 'html.parser')
 
-            campaign = get_campaign(campaigns, profile_list_html)
+            campaign = get_campaign_with_id(campaigns, profile_list_html)
 
             for profile_article in profile_list_html.find_all('article', class_='rowuser pos_rel cp'):
 
@@ -257,21 +292,33 @@ def run():
                                          cities=cities,
                                          campaign=campaign)
 
-                    #cv_path = download_and_get_cv_path(browser, user)
+                    if user.get(EMAIL) and candidate_not_sent(sent_candidates, user, campaign):
 
-                    parent = browser.find_element_by_id('cvCandidatePdf')
-                    parent.click()
+                        parent = browser.find_element_by_id('cvCandidatePdf')
+                        current_cv_path = get_last_download_path()
+                        parent.click()
 
-                    cv_path = get_last_download_path()
+                        cv_path = poll_for_last_download(current_cv_path)
 
-                    if cv_path:
-                        with open(cv_path, 'rb') as cv:
-                            response = requests.post(util.get_root_url() + '/api/v1/register',
-                                                     data=user,
-                                                     files={'curriculum_url': cv})
-                    else:  # no cv case
-                        response = requests.post(util.get_root_url() + '/api/v1/register', data=user)
-                    print('sent user with response: ' + str(response.status_code))
+                        if cv_path:
+                            with open(cv_path, 'rb') as cv:
+                                response = requests.post(util.get_root_url() + '/api/v1/register',
+                                                         data=user,
+                                                         files={'curriculum_url': cv})
+                        else:  # no cv case
+                            response = requests.post(util.get_root_url() + '/api/v1/register', data=user)
+
+                        print('user data:')
+                        print(user)
+                        if cv_path:
+                            print('user cv_path: ' + cv_path)
+                        print('sent user with response: ' + str(response.status_code))
+
+                        sent_candidates = sent_candidates.append([{EMAIL: user[EMAIL],
+                                                                   CAMPAIGN_ID: user.get(CAMPAIGN_ID),
+                                                                   KEY: get_key(user, campaign)}])
+
+    sent_candidates.to_csv(CSV_FILENAME, sep=',')
 
 
 if __name__ == '__main__':
