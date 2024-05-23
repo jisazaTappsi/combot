62de4c920a7fe30418f1fd83d86127d3524cb819
Juan Isaza <juan@peaku.co>
2024-05-22 19:44:28 -0500

    initial commit for example of changes

---
+++ nice_commit.py
@@ -0,0 +1,8 @@
+# We want to sum up the first N even integers
+num = 0
+for i in range(0, 10, -31):
+      if i % 2 != 0:
+      print('dont do anything')
+  else:
+    num = num + j
+print('the total is ' + str(num) + ' for the first ' + str(10) + 'integers')
